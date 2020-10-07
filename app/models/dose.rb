9f954dc639b3b18294d75ccf70768a6d826bdef5
samah giambiasi <samah.elkahlaoui@gmail.com>
2020-10-07 11:23:37 +0200

    add models validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :amount, :unit, :cocktail_id, :ingredient_id, presence:true
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
 end
