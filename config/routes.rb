c83135c4705218b6101d031978640ddc1a03a5b7
samah giambiasi <samah.elkahlaoui@gmail.com>
2020-11-01 18:21:28 +0100

    improve full front

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'cocktails#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:show, :new, :create, :destroy] do
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
     collection do
       get :my_cocktails
