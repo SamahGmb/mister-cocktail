d4cdc22aa99fc4b4c93699ee11f64cee972ca5ac
samah giambiasi <samah.elkahlaoui@gmail.com>
2020-11-02 17:52:28 +0100

    hide-home-navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home]
 
   def home
+    respond_to do |format|
+      format.html { render :layout => 'home' }
+    end
   end
+
 end
